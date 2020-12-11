db7f7fee08b5e56e368e54f7bbfa34503f5c277c
Zulkifli Sales <zulkiflisales@gmail.com>
2020-12-11 14:06:16 -0800

    revised work, still waiting for scatter plot.

--- app_server.R
+++ app_server.R
@@ -11,163 +11,159 @@ hospitals <- read.csv("scripts/Hospitals.csv")
 
 # Rishi's Work:
 
-#Variables
-#Number of total cases in WA state
+# Variables
+# Number of total cases in WA state
 total_cases <- RaceData %>%
-  filter(!is.na(Cases_Total)) %>% 
-  filter((State == "WA")) %>% 
-  filter((Date == "20201202")) %>% 
-  summarise(total = max(Cases_Total)) %>% 
+  filter(!is.na(Cases_Total)) %>%
+  filter((State == "WA")) %>%
+  filter((Date == "20201202")) %>%
+  summarise(total = max(Cases_Total)) %>%
   pull(total)
 
-#Number of total deaths in WA state
+# Number of total deaths in WA state
 total_deaths <- RaceData %>%
-  filter(!is.na(Deaths_Total)) %>% 
-  filter((State == "WA")) %>% 
-  summarise(total = max(Deaths_Total)) %>% 
+  filter(!is.na(Deaths_Total)) %>%
+  filter((State == "WA")) %>%
+  summarise(total = max(Deaths_Total)) %>%
   pull(total)
 
-#Number of total hospitalizations in WA state
+# Number of total hospitalizations in WA state
 total_hospitalizations <- RaceData %>%
-  filter(!is.na(Hosp_Total)) %>% 
-  filter((State == "WA")) %>% 
-  summarise(total = max(Hosp_Total)) %>% 
+  filter(!is.na(Hosp_Total)) %>%
+  filter((State == "WA")) %>%
+  summarise(total = max(Hosp_Total)) %>%
   pull(total)
 
-#Number of black cases in WA state
+# Number of black cases in WA state
 black_cases <- RaceData %>%
-  filter(!is.na(Cases_Black)) %>% 
-  filter((State == "WA")) %>% 
-  summarise(total = max(Cases_Black)) %>% 
+  filter(!is.na(Cases_Black)) %>%
+  filter((State == "WA")) %>%
+  summarise(total = max(Cases_Black)) %>%
   pull(total)
 
-#How many of the total cases are made up by black people in WA state
-# black_cases_percent <- trunc(max(black_cases)/total_cases *100)
-
-#Number of black deaths in WA state
+# Number of black deaths in WA state
 black_deaths <- RaceData %>%
-  filter(!is.na(Deaths_Black)) %>% 
-  filter((State == "WA")) %>% 
-  summarise(total = max(Deaths_Black)) %>% 
+  filter(!is.na(Deaths_Black)) %>%
+  filter((State == "WA")) %>%
+  summarise(total = max(Deaths_Black)) %>%
   pull(total)
 
-#Number of black hospitalizations in WA state
+# Number of black hospitalizations in WA state
 black_hospitalizations <- RaceData %>%
-  filter(!is.na(Hosp_Black)) %>% 
-  filter((State == "WA")) %>% 
-  summarise(total = max(Hosp_Black)) %>% 
+  filter(!is.na(Hosp_Black)) %>%
+  filter((State == "WA")) %>%
+  summarise(total = max(Hosp_Black)) %>%
   pull(total)
 
-#Number of white cases in WA state
+# Number of white cases in WA state
 white_cases <- RaceData %>%
-  filter((State == "WA")) %>% 
-  filter((Date == "20201202")) %>% 
-  filter(!is.na(Cases_White)) %>% 
-  summarise(total = max(Cases_White)) %>% 
+  filter((State == "WA")) %>%
+  filter((Date == "20201202")) %>%
+  filter(!is.na(Cases_White)) %>%
+  summarise(total = max(Cases_White)) %>%
   pull(total)
 
-#Number of white deaths in WA state
+# Number of white deaths in WA state
 white_deaths <- RaceData %>%
-  filter((State == "WA")) %>% 
-  filter(!is.na(Deaths_White)) %>% 
-  summarise(total = max(Deaths_White)) %>% 
+  filter((State == "WA")) %>%
+  filter(!is.na(Deaths_White)) %>%
+  summarise(total = max(Deaths_White)) %>%
   pull(total)
 
-#Number of white hospitalizations in WA state
+# Number of white hospitalizations in WA state
 white_hospitalizations <- RaceData %>%
-  filter(!is.na(Hosp_White)) %>% 
-  filter((State == "WA")) %>% 
-  summarise(total = max(Hosp_White)) %>% 
+  filter(!is.na(Hosp_White)) %>%
+  filter((State == "WA")) %>%
+  summarise(total = max(Hosp_White)) %>%
   pull(total)
 
-#Number of LatinX cases in WA state
+# Number of LatinX cases in WA state
 latinx_cases <- RaceData %>%
-  filter((State == "WA")) %>% 
-  filter(!is.na(Cases_LatinX)) %>% 
-  summarise(total = max(Cases_LatinX)) %>% 
+  filter((State == "WA")) %>%
+  filter(!is.na(Cases_LatinX)) %>%
+  summarise(total = max(Cases_LatinX)) %>%
   pull(total)
 
-#Number of LatinX deaths in WA state
+# Number of LatinX deaths in WA state
 latinx_deaths <- RaceData %>%
-  filter((State == "WA")) %>% 
-  filter(!is.na(Deaths_LatinX)) %>% 
-  summarise(total = max(Deaths_LatinX)) %>% 
+  filter((State == "WA")) %>%
+  filter(!is.na(Deaths_LatinX)) %>%
+  summarise(total = max(Deaths_LatinX)) %>%
   pull(total)
 
-#Number of white hospitalizations in WA state
+# Number of white hospitalizations in WA state
 latinx_hospitalizations <- RaceData %>%
-  filter(!is.na(Hosp_LatinX)) %>% 
-  filter((State == "WA")) %>% 
-  summarise(total = max(Hosp_LatinX)) %>% 
+  filter(!is.na(Hosp_LatinX)) %>%
+  filter((State == "WA")) %>%
+  summarise(total = max(Hosp_LatinX)) %>%
   pull(total)
 
-#Number of Asian cases in WA state
+# Number of Asian cases in WA state
 asian_cases <- RaceData %>%
-  filter((State == "WA")) %>% 
-  filter(!is.na(Cases_Asian)) %>% 
-  summarise(total = max(Cases_Asian)) %>% 
+  filter((State == "WA")) %>%
+  filter(!is.na(Cases_Asian)) %>%
+  summarise(total = max(Cases_Asian)) %>%
   pull(total)
 
-#Number of Asian deaths in WA state
+# Number of Asian deaths in WA state
 asian_deaths <- RaceData %>%
-  filter((State == "WA")) %>% 
-  filter(!is.na(Deaths_Asian)) %>% 
-  summarise(total = max(Deaths_Asian)) %>% 
+  filter((State == "WA")) %>%
+  filter(!is.na(Deaths_Asian)) %>%
+  summarise(total = max(Deaths_Asian)) %>%
   pull(total)
 
-#Number of Asian hospitalizations in WA state
+# Number of Asian hospitalizations in WA state
 asian_hospitalizations <- RaceData %>%
-  filter(!is.na(Hosp_Asian)) %>% 
-  filter((State == "WA")) %>% 
-  summarise(total = max(Hosp_Asian)) %>% 
+  filter(!is.na(Hosp_Asian)) %>%
+  filter((State == "WA")) %>%
+  summarise(total = max(Hosp_Asian)) %>%
   pull(total)
 
-#Number of American Indian or Alaskan Native cases in WA state
+# Number of American Indian or Alaskan Native cases in WA state
 aian_cases <- RaceData %>%
-  filter((State == "WA")) %>% 
-  filter(!is.na(Cases_AIAN)) %>% 
-  summarise(total = max(Cases_AIAN)) %>% 
+  filter((State == "WA")) %>%
+  filter(!is.na(Cases_AIAN)) %>%
+  summarise(total = max(Cases_AIAN)) %>%
   pull(total)
 
-#Number of American Indian or Alaskan Native deaths in WA state
+# Number of American Indian or Alaskan Native deaths in WA state
 aian_deaths <- RaceData %>%
-  filter((State == "WA")) %>% 
-  filter(!is.na(Deaths_AIAN)) %>% 
-  summarise(total = max(Deaths_AIAN)) %>% 
+  filter((State == "WA")) %>%
+  filter(!is.na(Deaths_AIAN)) %>%
+  summarise(total = max(Deaths_AIAN)) %>%
   pull(total)
 
-#Number of American Indian or Alaskan Native hospitalizations in WA state
+# Number of American Indian or Alaskan Native hospitalizations in WA state
 aian_hospitalizations <- RaceData %>%
-  filter(!is.na(Hosp_AIAN)) %>% 
-  filter((State == "WA")) %>% 
-  summarise(total = max(Hosp_AIAN)) %>% 
+  filter(!is.na(Hosp_AIAN)) %>%
+  filter((State == "WA")) %>%
+  summarise(total = max(Hosp_AIAN)) %>%
   pull(total)
 
-#Number of Native Hawaiian and Pacific Islander cases in WA state
+# Number of Native Hawaiian and Pacific Islander cases in WA state
 nhpi_cases <- RaceData %>%
-  filter((State == "WA")) %>% 
-  filter(!is.na(Cases_NHPI)) %>% 
-  summarise(total = max(Cases_NHPI)) %>% 
+  filter((State == "WA")) %>%
+  filter(!is.na(Cases_NHPI)) %>%
+  summarise(total = max(Cases_NHPI)) %>%
   pull(total)
 
-#Number of Native Hawaiian and Pacific Islander deaths in WA state
+# Number of Native Hawaiian and Pacific Islander deaths in WA state
 nhpi_deaths <- RaceData %>%
-  filter((State == "WA")) %>% 
-  filter(!is.na(Deaths_NHPI)) %>% 
-  summarise(total = max(Deaths_NHPI)) %>% 
+  filter((State == "WA")) %>%
+  filter(!is.na(Deaths_NHPI)) %>%
+  summarise(total = max(Deaths_NHPI)) %>%
   pull(total)
 
-#Number of Native Hawaiian and Pacific Islander hospitalizations in WA state
+# Number of Native Hawaiian and Pacific Islander hospitalizations in WA state
 nhpi_hospitalizations <- RaceData %>%
-  filter(!is.na(Hosp_NHPI)) %>% 
-  filter((State == "WA")) %>% 
-  summarise(total = max(Hosp_NHPI)) %>% 
+  filter(!is.na(Hosp_NHPI)) %>%
+  filter((State == "WA")) %>%
+  summarise(total = max(Hosp_NHPI)) %>%
   pull(total)
 
-races <- function(race) { 
+races <- function(race) {
   df <- filter(RaceData, State == "WA")
   race_num <- df[[race]]
-  #print(df)
   number <- as.numeric(race_num)
   max_num <- max(number, na.rm = TRUE)
   return(max_num)
@@ -176,46 +172,46 @@ races <- function(race) {
 
 
 viz <- function(input) {
-  plan <- RaceData %>% gather("type", "n", 3:ncol(RaceData)) %>% filter(!is.na(n)) %>% filter(State == "WA")
-  #View(plan)
+  plan <- RaceData %>%
+    gather("type", "n", 3:ncol(RaceData)) %>%
+    filter(!is.na(n)) %>%
+    filter(State == "WA")
   plan$Date <- as.character(plan$Date)
-  plan$Date <- as.Date(plan$Date, '%Y%m%d')
+  plan$Date <- as.Date(plan$Date, "%Y%m%d")
   plan$n <- as.numeric(plan$n)
-  #print(plan[['n']])
   filter_specific <- plan %>% filter(type %in% input)
-  
-  v <- ggplot(filter_specific) + geom_line(aes(x = Date, y = n, group = type, color = type))
+
+  v <- ggplot(filter_specific) +
+    geom_line(aes(x = Date, y = n, group = type, color = type))
   v <- ggplotly(v)
   return(v)
 }
 
-# Muhammad's Work
+# Muhammad's Work:
 WA_owners <- hospitals %>%
   filter(STATE == "WA") %>%
   group_by(OWNER, COUNTY) %>%
   summarize(length(COUNTY))
-names(WA_owners)[names(WA_owners) == 'length(COUNTY)'] <- 'Hospitals'
+names(WA_owners)[names(WA_owners) == "length(COUNTY)"] <- "Hospitals"
 
 
 # Define server function
 server <- function(input, output) {
-  
   output$viz <- renderPlotly({
     viz(input$checkGroup)
   })
-  
-  output$analysis <- renderText ({
-    #paste0("")
+
+  output$analysis <- renderText({
+    # paste0("")
   })
-  
+
   output$boxplot <- renderPlotly({
-    
-    plot <- WA_owners %>% 
+    plot <- WA_owners %>%
       filter(COUNTY == input$sidebar_content)
-    
-    ggplot(plot, mapping = aes_string(x = 'OWNER', y = 'Hospitals')) +
-      geom_bar(stat='identity') + 
-      coord_cartesian(ylim=c(0,20)) +
+
+    ggplot(plot, mapping = aes_string(x = "OWNER", y = "Hospitals")) +
+      geom_bar(stat = "identity") +
+      coord_cartesian(ylim = c(0, 20)) +
       labs(title = "Hospitals", x = "Ownership", y = "Number of Hospitals")
   })
 }
--- app_ui.R
+++ app_ui.R
@@ -16,7 +16,7 @@ WA_owners <- hospitals %>%
   filter(STATE == "WA") %>%
   group_by(OWNER, COUNTY) %>%
   summarize(length(COUNTY))
-names(WA_owners)[names(WA_owners) == 'length(COUNTY)'] <- 'Hospitals'
+names(WA_owners)[names(WA_owners) == "length(COUNTY)"] <- "Hospitals"
 
 
 select_values <- unique(WA_owners$COUNTY)
@@ -24,101 +24,138 @@ select_values <- unique(WA_owners$COUNTY)
 sidebar_content <- selectInput(
   inputId = "sidebar_content",
   label = "County",
-  choices = select_values)
+  choices = select_values
+)
 
 main_content <- mainPanel(
-  
   plotlyOutput("boxplot"),
-  
+
   p(
-    "Three counties do not have any hospitals at all. They are Douglas, Skamania, and Wahkiakum."
+    "Three counties do not have any hospitals at all. They are Douglas,
+    Skamania, and Wahkiakum."
   )
 )
 
 # Define the UI and what pages/tabs go into it
-ui <- fluidPage(theme = shinytheme("superhero"),
+ui <- fluidPage(
+  theme = shinytheme("superhero"),
   navbarPage(
-  "Washington State Health and Resources",
-  tabPanel("Introduction",
-           mainPanel(
-             h1("Introdution:"),
-             p("Our dataset is used to recognize differing COVID-19 datasets across 
-      counties in Washington State. We are attempting to analyze the differing
-      number & type of hospitals in each county, positive cases per county, and 
-      Hopspitalizations types and cases by race in counties with varying populations,
-      demographics, and incomes. With this we intend to analyze which counties
-      may have disadvantages over others when it comes to dealing with COVID,
-      due to differing levels of access to healthcare resources per county.
-      This helps meet our overarching goal of combating health disparities in 
-      Washington State by providing dataset analyses comparing how COVID has 
-      impacted different people and cities.", align = 'center'),
-             p("The data's we used were provided from the Department of Homeland
-               Security (Foundation-Level Data), and the foundation of something
-               something something something something"
-               , align = 'right'),
-             tags$img(src = "uwmedcenter.png", align = 'center'),
-             p("UW Medical Center ||
-               source: http://www.dailyuw.com/news/article_ec64c80c-b4f7-11ea-bed1-4ffbbfd1cb64.html"
-               , align = 'center')
-             ),
-           ),
-  tabPanel("Line Chart",
-           
-             
-             # Application title
-             titlePanel("Number of Covid Cases per Race in WA State"),
-             
-             # Sidebar with a slider input for number of bins 
-             sidebarLayout(
-               sidebarPanel(
-                 checkboxGroupInput("checkGroup", label = h3("Please Choose the Race/Races You Want to See the Data for"), 
-                                    choices = list("White" = "Cases_White",
-                                                   "Black" = "Cases_Black",
-                                                   "LatinX" = "Cases_LatinX",
-                                                   "Asian" = "Cases_Asian",
-                                                   "AIAN" = "Cases_AIAN",
-                                                   "NHPI" = "Cases_NHPI"),
-                                    selected = "Cases_White")
-               ),
-               
-               # Show a plot of the generated distribution
-               mainPanel(
-                 plotlyOutput("viz"),
-                 textOutput('analysis')
-               )
-             )
+    "Washington State Health and Resources",
+    tabPanel(
+      "Introduction",
+      mainPanel(
+        h1("Introdution:", align = "center"),
+        p("Our dataset is used to recognize differing COVID-19 datasets
+             across counties in Washington State. We are attempting to analyze
+             the differing number & type of hospitals in each county, positive
+             cases per county, and Hopspitalizations types and cases by race in
+             counties with varying populations, demographics, and incomes. With
+             this we intend to analyze which counties may have disadvantages
+             over others when it comes to dealing with COVID, due to differing
+             levels of access to healthcare resources per county. This helps
+             meet our overarching goal of combating health disparities in
+             Washington State by providing dataset analyses comparing how COVID
+             has impacted different people and cities.", align = "center"),
+        br(),
+        h3("Data-Driven Questions:", align = "center"),
+        p("The questions that we set out to answer are: ",
+          br(),
+          strong("Are there health disparities present between different 
+                 ethnicities/races that lead them to have poor healthcare as 
+                 compared to others?"),
+          br(),
+          strong("Is there a correaltion between the number of hospitalization and
+                 number of COVID cases in Washington?"),
+          br(),
+          strong("How many and what types of hospitals are in the counties
+                 of Washington state?"),
+          align = "center"
         ),
-  tabPanel("Peyton",
-           p("work in progress")),
-  tabPanel(
-    "Boxplot",
-    
-    titlePanel("Number and Type of Hospitals in WA Counties"),
-    
-    sidebarLayout(
-      sidebarPanel(
-        sidebar_content),
-      main_content
-    )
-  ),
-  tabPanel("Summary",
-           p("Using data from the Department of Homeland Security and the New 
-             York Times, we determined that receiving information on hospitals 
-             and relating information (such as number of beds or any pertinent 
-             information on the hospital) would be best for our analysis on the 
-             state of Washington. We calculated the total unique type of 
-             ownership a hospital in WA could have (6), we also calculated the 
+        br(),
+        h3("Datsets:", align = "center"),
+        p("The data sets we used were provided from the",
+      em("Department of Homeland Security (Foundation-Level Data), and The
+         COVID Tracking Project"),
+        "and these data sets will aid us in our analysis to answer the questions while using a",
+      strong("line chart, scatter plot, and bar chart"), 
+      "to further support our findings.",
+          align = "center"
+        ),
+        tags$img(src = "uwmedcenter.png", align = "center"),
+        p("UW Medical Center", align = "center")
+      ),
+    ),
+    tabPanel(
+      "Number of COVID cases per race",
+
+
+      # Application title
+      titlePanel("Number of Covid Cases per Race in WA State"),
+
+      # Sidebar with a slider input for number of bins
+      sidebarLayout(
+        sidebarPanel(
+          checkboxGroupInput("checkGroup",
+            label = h3("Please Choose the
+                                      Race/Races You Want to See the Data for"),
+            choices = list(
+              "White" = "Cases_White",
+              "Black" = "Cases_Black",
+              "LatinX" = "Cases_LatinX",
+              "Asian" = "Cases_Asian",
+              "AIAN" = "Cases_AIAN",
+              "NHPI" = "Cases_NHPI"
+            ),
+            selected = "Cases_White"
+          )
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+          plotlyOutput("viz"),
+          textOutput("analysis")
+        )
+      )
+    ),
+    tabPanel(
+      "Current Hospitalizatoins",
+      p("work in progress")
+    ),
+    tabPanel(
+      "Type of Hospitals in Counties",
+
+      titlePanel("Number and Type of Hospitals in WA Counties"),
+
+      sidebarLayout(
+        sidebarPanel(
+          sidebar_content
+        ),
+        main_content
+      )
+    ),
+    tabPanel(
+      "Summary",
+      h2("Summary Analysis:"),
+      p("Using data from the Department of Homeland Security and the New
+             York Times, we determined that receiving information on hospitals
+             and relating information (such as number of beds or any pertinent
+             information on the hospital) would be best for our analysis on the
+             state of Washington. We calculated the total unique type of
+             ownership a hospital in WA could have (6), we also calculated the
              mean (3.667) and median (2) amount of hospitals in each county from
-             the data given. We found that there were 36 counties in WA with at 
-             least one hospital, and there are 39 counties total meaning 3 
-             counties have no hospitals at all. We also provided a box and 
-             whisker plot for the number of hospitals per county, a scatterplot 
-             for the number of positive COVID cases vs current number of 
-             hospitalizations, and a bar chart of the number of hospitals vs 
-             number of deaths. We believed that calculating these values are 
+             the data given. We found that there were 36 counties in WA with at
+             least one hospital, and there are 39 counties total meaning 3
+             counties have no hospitals at all. We also provided a box and
+             whisker plot for the number of hospitals per county, a scatterplot
+             for the number of positive COVID cases vs current number of
+             hospitalizations, and a bar chart of the number of hospitals vs
+             number of deaths. We believed that calculating these values are
              most pertinent for us to determine the degree to which COVID-19 has
              affected the state of Washington."),
-           tags$img(src = "mask.png", align = 'right')
+      tags$img(
+        src = "mask.png", align = "right", height = 800,
+        width = 800
+      )
     )
   )
 )
